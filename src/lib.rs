6b3d319fd0c4952a0389f544255124e4b24f41c1
DTSmth <ducktapedave85@gmail.com>
2025-10-06 13:25:56 -0400

    rust book 7.5 Separating Modules into Different Files

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
-mod front_of_house {
-    use crate::{back_of_house, front_of_house};
-
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
+mod front_of_house;
+pub use crate::front_of_house::hosting;
 
     pub fn eat_at_restaurant() {
         crate::front_of_house::hosting::add_to_waitlist();
@@ -25,7 +11,7 @@ mod front_of_house {
 
         front_of_house::hosting::add_to_waitlist();
     }
-}
+
 
 mod back_of_house {
     use crate::front_of_house::hosting;
